8c31c8d2560a70bb9ab771ff922f8d64b772ef1d
Nathan Broadbent <nathan.f77@gmail.com>
2011-09-13 16:53:01 +0800

    Added test for generated/specified text parts

--- test/inline_css_hook_test.rb
+++ test/inline_css_hook_test.rb
@@ -1,11 +1,6 @@
 require 'abstract_unit'
 
-class HelperMailer < ActionMailer::Base
-  helper ActionMailer::InlineCssHelper
-
-  def use_inline_css_hook
-    mail_with_defaults do |format|
-      format.html { render(:inline => %Q{
+TEST_HTML = %Q{
 <html>
   <head>
     <style>
@@ -15,9 +10,21 @@ class HelperMailer < ActionMailer::Base
   <body>
     <div id="test">Test</div>
   </body>
-</html>
-}) }
-      format.text { render(:inline => "Text Part") }
+</html>}
+
+class HelperMailer < ActionMailer::Base
+  helper ActionMailer::InlineCssHelper
+
+  def use_inline_css_hook_with_only_html_part
+    mail_with_defaults do |format|
+      format.html { render(:inline => TEST_HTML) }
+    end
+  end
+
+  def use_inline_css_hook_with_text_and_html_parts
+    mail_with_defaults do |format|
+      format.html { render(:inline => TEST_HTML) }
+      format.text { render(:inline => "Different Text Part") }
     end
   end
 
@@ -29,10 +36,20 @@ class HelperMailer < ActionMailer::Base
   end
 end
 
+
 class InlineCssHookTest < ActionMailer::TestCase
-  def test_inline_css_hook
-    mail = HelperMailer.use_inline_css_hook.deliver
+  def test_inline_css_hook_with_only_html_part
+    mail = HelperMailer.use_inline_css_hook_with_only_html_part.deliver
     assert_match '<div id="test" style="color: #123456;">Test</div>', mail.html_part.body.encoded
+    # Test generated text part
+    assert_match 'Test', mail.text_part.body.encoded
+  end
+
+  def test_inline_css_hook_with_text_and_html_parts
+    mail = HelperMailer.use_inline_css_hook_with_text_and_html_parts.deliver
+    assert_match '<div id="test" style="color: #123456;">Test</div>', mail.html_part.body.encoded
+    # Test specified text part
+    assert_match 'Different Text Part', mail.text_part.body.encoded
   end
 end
 
