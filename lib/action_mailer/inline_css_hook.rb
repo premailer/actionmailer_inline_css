49ea3c9273810268ae8d4e01adf8137105135ed5
Nathan Broadbent <nathan.f77@gmail.com>
2012-01-06 21:26:22 +0800

    Fixed actionmailer hook to generate text parts (tested), as well as re-attaching existing attachments

--- lib/action_mailer/inline_css_hook.rb
+++ lib/action_mailer/inline_css_hook.rb
@@ -1,20 +1,24 @@
-# encoding: utf-8
 #
 # Always inline CSS for HTML emails
 #
 module ActionMailer
   class InlineCssHook
     def self.delivering_email(message)
-
       if html_part = (message.html_part || (message.content_type =~ /text\/html/ && message))
-
         # Generate an email with all CSS inlined (access CSS a FS path)
-        premailer = ::Premailer.new(html_part.body.to_s, :with_html_string => true)
+        premailer = ::Premailer.new(html_part.body.to_s,
+                                    :with_html_string => true)
 
         # Prepend host to remaning URIs.
         # Two-phase conversion to avoid request deadlock from dev. server (Issue #4)
-        premailer = ::Premailer.new(premailer.to_inline_css, :with_html_string => true,
-                                                             :base_url => message.header[:host].to_s)
+        premailer = ::Premailer.new(premailer.to_inline_css,
+                                      :with_html_string => true,
+                                      :base_url => message.header[:host].to_s)
+        existing_text_part = message.text_part && message.text_part.body.to_s
+        existing_attachments = message.attachments
+
+        # Reset the body
+        message.body = nil
 
         # Add an HTML part with CSS inlined.
         message.html_part do
@@ -22,12 +26,17 @@ module ActionMailer
           body premailer.to_inline_css
         end
 
+        # Add a text part with either the pre-existing text part, or one generated with premailer.
+        message.text_part do
+          content_type "text/plain; charset=utf-8"
+          body existing_text_part || premailer.to_plain_text
+        end
+
+        # Re-add any attachments
+        existing_attachments.each {|a| message.body << a }
+
+        message
       end
-
-      # Return the message, ActionMailer doesn't seem to care, but it's useful
-      # for writing meaningful tests.
-      message
-
     end
   end
 end
