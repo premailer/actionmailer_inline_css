39fbd1d8d3f3ebad04bf306f70d1ab832c77b361
Adam LaFave <lafave.adam@gmail.com>
2013-10-29 16:25:46 -0700

    refactor InlineCssHook

--- lib/action_mailer/inline_css_hook.rb
+++ lib/action_mailer/inline_css_hook.rb
@@ -3,60 +3,110 @@
 #
 module ActionMailer
   class InlineCssHook
-    def self.delivering_email(message)
 
-      if html_part = (message.html_part || (message.content_type =~ /text\/html/ && message))
-        host = ActionMailerInlineCss.base_url || message.header[:host].to_s
+    # After registering ActionMailer::InlineCssHook as an interceptor, this
+    # gets called prior to sending emails.
+    def self.delivering_email(*args)
+      new(*args).delivering_email
+    end
 
-        # Generate an email with all CSS inlined (access CSS a FS path), and URIs
-        premailer = ::Premailer.new(html_part.body.to_s, :with_html_string => true, :base_url => host)
+    # @param message [Mail::Message]
+    #
+    # @return [ActionMailer::InlineCssHook]
+    def initialize(message)
+      @message     = message
+      @attachments = @message.attachments
+      @charset     = @message.charset
+      @host        = ActionMailerInlineCss.base_url || @message.header[:host].to_s
+    end
 
-        msg_charset = message.charset
+    # @return [Mail::Message]
+    def delivering_email
+      if html_part = (@message.html_part || (@message.content_type =~ /text\/html/ && @message))
+        premailer = ::Premailer.new(html_part.body.to_s, with_html_string: true, base_url: @host)
 
-        if message.text_part && message.text_part.body.to_s
-          html_part.content_type "text/html; charset=#{msg_charset}"
+        # Use the existing text part if it exists, otherwise build one.
+        if @message.text_part && @message.text_part.body.to_s
+          html_part.content_type "text/html; charset=#{@charset}"
           html_part.body premailer.to_inline_css
+          @message.add_part nest_header_parts(@message.text_part, html_part)
         else
-          existing_attachments = message.attachments
-
-          # Clear body to make a multipart email
-          message.body = nil
-
-          # Important: Plain text part must be generated before the CSS is
-          # inlined. Not doing so results in CSS declarations being visible
-          # in the plain text part.
-          text_alternative = Mail::Part.new do
-            content_type "text/plain; charset=#{msg_charset}"
-            body premailer.to_plain_text
-          end
-
-          html_alternative = Mail::Part.new do
-            content_type "text/html; charset=#{msg_charset}"
-            body premailer.to_inline_css
-          end
-
-          html_container        = Mail::Part.new { content_type "multipart/related" }
-          alternative_container = Mail::Part.new { content_type "multipart/alternative" }
-
-          alternative_container.add_part text_alternative
-          alternative_container.add_part html_alternative
-
-          message.add_part alternative_container
-
-          # Change the content type to `multipart/mixed` while preserving
-          # additional parameters such as `boundary`, `charset`, etc. if there
-          # are any email attachments.
-          unless existing_attachments.empty?
-            content_type    = message.content_type.split(";")
-            content_type[0] = "multipart/mixed"
-            message.content_type content_type.join(";")
-          end
-
-          existing_attachments.each {|a| message.body << a }
+          @message.body = nil
+          @message.add_part nest_header_parts(build_text_part(premailer), build_html_part(premailer))
         end
 
-        message
+        add_attachments unless @attachments.empty?
+        @message
       end
+    end
+
+    private
+
+    # Change the content type to `multipart/mixed` while preserving aditional
+    # parameters such as `boundary`, `charset`, etc.
+    #
+    # @return [void]
+    def add_attachments
+      content_type    = @message.content_type.split(";")
+      content_type[0] = "multipart/mixed"
+      @message.content_type content_type.join(";")
+
+      @attachments.each { |a| @message.body << a }
+    end
+
+    # @param premailer [Premailer]
+    #
+    # @return [Mail::Part]
+    def build_html_part(premailer)
+      Mail::Part.new do
+        content_type "text/html; charset=#{@charset}"
+        body premailer.to_inline_css
+      end
+    end
+
+    # @note Plain text part must be generated before the CSS is inlined. Not
+    #   doing so results in CSS declarations being visible in the plain text
+    #   part.
+    #
+    # @param premailer [Premailer]
+    #
+    # @return [Mail::Part]
+    def build_text_part(premailer)
+      Mail::Part.new do
+        content_type "text/plain; charset=#{@charset}"
+        body premailer.to_plain_text
+      end
+    end
+
+    # Take two mail parts that belong at different levels in the MIME header
+    # part hierachy and nest them appropriately. For reference, this is the
+    # hierarchy:
+    #
+    #   multipart/mixed
+    #     multipart/related
+    #       multipart/alternative
+    #         text/plain
+    #         text/html
+    #       image/jpeg (example) (inline attachments)
+    #     aplication/pdf (example) (normal attachments)
+    #
+    # @param text_part [Mail::Part]
+    # @param html_part [Mail::Part]
+    #
+    # @return [Mail::Part]
+    def nest_header_parts(text_part, html_part)
+      # Nest parts with content types `text/plain` and `text/html` inside of a
+      # part with content type `multipart/alternative`.
+      alternative_part = Mail::Part.new { content_type "multipart/alternative" }
+      alternative_part.add_part text_part
+      alternative_part.add_part html_part
+
+      # Nest part with content type `multipart/alternative` inside of a part
+      # with content type `multipart/related`.
+      related_part = Mail::Part.new { content_type "multipart/related" }
+      related_part.add_part alternative_part
+
+      related_part
     end
   end
 end
